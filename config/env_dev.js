5392104c9340d669df452a04a91f6c23ad2ba004
Aleksandar Nikolic <salex.nikolic@gmail.com>
2019-12-05 19:44:13 +0100

    Delete env_dev.js

--- config/env_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {   					 
-     sendgridApiKey: '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',                             
-     sessionSecret: 'my secret'
-}
