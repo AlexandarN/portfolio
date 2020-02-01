5f2222ad239a5d268f10c2319b3be82a00098348
Aleksandar Nikolic <salex.nikolic@gmail.com>
2020-02-01 19:05:30 +0100

    phone calls enabled 4

--- config/env_dev.js
+++ config/env_dev.js
@@ -1,4 +1,22 @@
 module.exports = {   					 
      sendgridApiKey: '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',                             
-     sessionSecret: 'my secret'
+     sessionSecret: 'my secret',
+     TWILIO_ACCOUNT_SID: '',
+     TWILIO_AUTH_TOKEN: '',
+     TWILIO_NUMBER: ''
 }
+
+// "env": {
+//      "TWILIO_ACCOUNT_SID": {
+//        "description": "Your Twilio account SID",
+//        "value": "enter_your_account_sid_here"
+//      },
+//      "TWILIO_AUTH_TOKEN": {
+//        "description": "Your Twilio account Auth token,      
+//        "value": "enter_your_auth_token_here"
+//      },
+//      "TWILIO_NUMBER": {
+//        "description": "Twilio phone number you are using for this app. 
+//        "value": "+15005550006"
+//      }
+//    }
--- config/env_prod.js
+++ config/env_prod.js
@@ -1,4 +1,7 @@
 module.exports = {   					        
      sendgridApiKey: process.env.SENDGRID_API_KEY,
-     sessionSecret: process.env.SESSION_SECRET
+     sessionSecret: process.env.SESSION_SECRET,
+     TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+     TWILIO_AUTH_TOKEN: proces.env.TWILIO_AUTH_TOKEN,
+     TWILIO_NUMBER: process.env.TWILIO_NUMBER
 }
