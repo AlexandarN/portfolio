aac4f5b70c7e6f77dbafb5e9294dad1553917eed
Aleksandar Nikolic <salex.nikolic@gmail.com>
2020-02-01 19:07:23 +0100

    phone calls enabled 4

--- config/env_prod.js
+++ config/env_prod.js
@@ -1,7 +1,4 @@
 module.exports = {   					        
      sendgridApiKey: process.env.SENDGRID_API_KEY,
-     sessionSecret: process.env.SESSION_SECRET,
-     TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-     TWILIO_AUTH_TOKEN: proces.env.TWILIO_AUTH_TOKEN,
-     TWILIO_NUMBER: process.env.TWILIO_NUMBER
+     sessionSecret: process.env.SESSION_SECRET
 }
